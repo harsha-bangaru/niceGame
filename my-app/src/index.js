844a526921de5c5af2f2fc27c0df1ca3586cf2b0
DisasterMaster3 <harsha.bangaru99@gmail.com>
2022-02-16 22:39:25 -0500

    install express

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -11,7 +11,10 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-const express = require('express');const app = express();const http = require('http');const server = http.createServer(app);
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
 app.get('/', (req, res) => {  res.send('<h1>Hello world</h1>');});
 server.listen(3000, () => {  console.log('listening on *:3000');});
 
